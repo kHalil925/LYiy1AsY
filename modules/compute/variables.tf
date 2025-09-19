1759e2e7a1f7a4e0ff9a8a596f68cc241ee8e31d
kHalil925 <works-bandeau0n@icloud.com>
2025-09-19 18:17:36 +0100

    重构了 main.py 代码

---
+++ modules/compute/variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" {}
+variable "location" {}
+variable "vm_name" {}
+variable "vm_size" {}
+variable "admin_username" {}
+variable "admin_password" {}
+variable "workload_script" {}
